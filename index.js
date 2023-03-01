c57dd2402c01decdf132135a9bbe95448d7eed05
Caleb Lockwood <caleblockwood@iMac.local>
2023-02-28 23:56:18 -0700

    corrected the choices in the lisence question. I had placed the questions as a string instead of an array

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const questions = [
         message: "What license would you prefer to use for this project?",
         name: "license",
         default: "MIT",
-        choices: "['MIT', 'Apache License 2.0', 'Berkeley Software Distribution', 'GPL v3', 'MPL 2.0']",
+        choices: ['MIT', 'Apache License 2.0', 'Berkeley Software Distribution', 'GPL v3', 'MPL 2.0'],
     },
     {
         type: "input",
