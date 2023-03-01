104a243dbd0a1c4f481418509fd6138c6e0f25eb
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-01 00:40:36 -0700

    wrote the renderLicenseBadge function in generate Markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'Apache License 2.0':
+      return '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    case 'Berkeley Software Distribution':
+      return '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+    case 'GPL v3':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    case 'MPL 2.0':
+      return '![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
